d19b989c18bfad28bc8d6a0bf21fd6e82a756d11
howdycrystalm <howdycrystalm@gmail.com>
2018-06-10 21:54:44 -0700

    established components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ResourcesComponent } from './components/resources/resources.component';
+import { SurvivorStoriesComponent } from './components/survivor-stories/survivor-stories.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ResourcesComponent,
+    SurvivorStoriesComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/survivor-stories/survivor-stories.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-survivor-stories',
+  templateUrl: './survivor-stories.component.html',
+  styleUrls: ['./survivor-stories.component.css']
+})
+export class SurvivorStoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
