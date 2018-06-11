af61f1fb0025b7b3e824913d3f2b60ad1f08b384
howdycrystalm <howdycrystalm@gmail.com>
2018-06-10 22:21:40 -0700

    added rest of paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './components/home/home.component'
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ResourcesComponent } from './components/resources/resources.component';
+import { SurvivorStoriesComponent } from './components/survivor-stories/survivor-stories.component';
+
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent}, //this will make the home component index by default
+  {path: 'login', component: LoginComponent},
+  {path: 'navbar', component: NavbarComponent},
+  {path: 'resources', component: ResourcesComponent},
+  {path: 'survivor-stories', component: SurvivorStoriesComponent}
 ];
 
 @NgModule({
